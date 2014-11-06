5e8b8791533533002bdd53136eff89000eb077cd
zeird <l1aqus@gmail.com>
2014-11-07 01:50:23 +0600

    Refactor forum.js, add more formatting buttons

--- source/forum.js
+++ source/forum.js
@@ -2,6 +2,23 @@
 'use strict';
 try {
 
+var doc = document;
+var $id = function(id) {
+	return doc.getElementById(id);
+};
+var $C = function(classname) {
+	return doc.getElementsByClassName(classname);
+};
+var $c = function(classname) {
+	return doc.getElementsByClassName(classname)[0];
+};
+var $Q = function(sel) {
+	return doc.querySelectorAll(sel);
+};
+var $q = function(sel) {
+	return doc.querySelector(sel);
+};
+
 var storage = {
 	_get_key: function(key) {
 		return "GM_" + god_name + ':' + key;
@@ -17,23 +34,23 @@ var storage = {
 
 var i, len, follow_links, isFollowed, links_containers, temp, topic, unfollow_links,
 	isTopic = location.pathname.match(/topic/) !== null,
-	forum_topics = 'Forum' + (isTopic ? document.querySelector('.crumbs a:nth-child(3)').href.match(/forums\/show\/(\d+)/)[1]
+	forum_topics = 'Forum' + (isTopic ? $q('.crumbs a:nth-child(3)').href.match(/forums\/show\/(\d+)/)[1]
 									  : location.pathname.match(/forums\/show\/(\d+)/)[1]),
 	god_name = localStorage.getItem('GM_CurrentUser'),
 	topics = JSON.parse(storage.get(forum_topics));
 
 if (isTopic) {
-	links_containers = document.querySelectorAll('#topic_mod');
+	links_containers = $Q('#topic_mod');
 } else {
 	// add missing <small> elements
-	temp = document.querySelectorAll('.c2');
+	temp = $Q('.c2');
 	for (i = 0, len = temp.length; i < len; i++) {
 		if (!temp[i].querySelector('small')) {
 			temp[i].insertAdjacentHTML('beforeend', '<small />');
 		}
 	}
 
-	links_containers = document.querySelectorAll('.c2 small');
+	links_containers = $Q('.c2 small');
 }
 
 // add links
@@ -46,34 +63,42 @@ for (i = 0, len = links_containers.length; i < len; i++) {
 }
 
 // add click events to follow links
-follow_links = document.querySelectorAll('.follow');
+follow_links = $Q('.follow');
 var follow = function(e) {
-	e.preventDefault();
-	var topic = isTopic ? location.pathname.match(/\d+/)[0]
-						: this.parentElement.parentElement.querySelector('a').href.match(/\d+/)[0],
-		posts = isTopic ? +document.querySelector('.subtitle').textContent.match(/\d+/)[0]
-						: +this.parentElement.parentElement.nextElementSibling.textContent,
-		topics = JSON.parse(storage.get(forum_topics));
-	topics[topic] = posts;
-	storage.set(forum_topics, JSON.stringify(topics));
-	this.style.display = 'none';
-	this.parentElement.querySelector('.unfollow').style.display = 'inline';
+	try {
+		e.preventDefault();
+		var topic = isTopic ? location.pathname.match(/\d+/)[0]
+							: this.parentElement.parentElement.querySelector('a').href.match(/\d+/)[0],
+			posts = isTopic ? +$('.subtitle').textContent.match(/\d+/)[0]
+							: +this.parentElement.parentElement.nextElementSibling.textContent,
+			topics = JSON.parse(storage.get(forum_topics));
+		topics[topic] = posts;
+		storage.set(forum_topics, JSON.stringify(topics));
+		this.style.display = 'none';
+		this.parentElement.querySelector('.unfollow').style.display = 'inline';
+	} catch(error) {
+		console.error(error);
+	}
 };
 for (i = 0, len = follow_links.length; i < len; i++) {
 	follow_links[i].onclick = follow;
 }
 
 // add click events to unfollow links
-unfollow_links = document.querySelectorAll('.unfollow');
+unfollow_links = $Q('.unfollow');
 var unfollow = function(e) {
-	e.preventDefault();
-	var topic = isTopic ? location.pathname.match(/\d+/)[0]
-						: this.parentElement.parentElement.querySelector('a').href.match(/\d+/)[0],
-		topics = JSON.parse(storage.get(forum_topics));
-	delete topics[topic];
-	storage.set(forum_topics, JSON.stringify(topics));
-	this.style.display = 'none';
-	this.parentElement.querySelector('.follow').style.display = 'inline';
+	try {
+		e.preventDefault();
+		var topic = isTopic ? location.pathname.match(/\d+/)[0]
+							: this.parentElement.parentElement.querySelector('a').href.match(/\d+/)[0],
+			topics = JSON.parse(storage.get(forum_topics));
+		delete topics[topic];
+		storage.set(forum_topics, JSON.stringify(topics));
+		this.style.display = 'none';
+		this.parentElement.querySelector('.follow').style.display = 'inline';
+	} catch(error) {
+		console.error(error);
+	}
 };
 for (i = 0, len = unfollow_links.length; i < len; i++) {
 	unfollow_links[i].onclick = unfollow;
@@ -82,24 +107,69 @@ for (i = 0, len = unfollow_links.length; i < len; i++) {
 // scroll to a certain post #
 var guip_hash = location.hash.match(/#guip_(\d+)/);
 if (guip_hash) {
-	location.hash = document.getElementsByClassName('spacer')[+guip_hash[1]].id;
+	location.hash = $C('spacer')[+guip_hash[1]].id;
 }
 
 // formatting buttons
-var $reply_form = document.getElementById('post_body_editor');
+var $reply_form = $id('post_body_editor');
 console.log($reply_form);
 if ($reply_form) {
 	window.GUIp_addGlobalStyleURL('forum.css', 'forum_css');
 	$reply_form.insertAdjacentHTML(
-		'beforebegin', 
-		'<a class="formatting bold" title="Сделать полужирным">Ж</a>' +
-		'<a class="formatting underline" title="Подчеркнуть">П</a>' +
-		'<a class="formatting strike" title="Зачеркнуть">З</a>' +
-		'<a class="formatting italic" title="Сделать курсивным">К</a>'
+		'afterbegin', 
+		'<a class="formatting button bold" title="Сделать полужирным">Ж</a>' +
+		'<a class="formatting button underline" title="Подчеркнуть">П</a>' +
+		'<a class="formatting button strike" title="Зачеркнуть">З</a>' +
+		'<a class="formatting button italic" title="Сделать курсивным">К</a>' +
+		'<blockquote class="formatting bq" title="Процитировать">bq.</blockquote>' +
+		'<pre class="formatting bc" title="Выделить"><code>bc.</code></pre>' +
+		'<a class="formatting button godname" title="Вставить ссылку на бога"></a>' +
+		'<a class="formatting button link" title="Вставить ссылку">a</a>'
 	);
+	var val, ss, se, pb = $id('post_body');
+	var basic_formatting = function(left, right, e) {
+		try {
+			val = pb.value;
+			ss = pb.selectionStart;
+			se = pb.selectionEnd;
+			while (ss < se && val[ss].match(/\s/)) {
+				ss++;
+			}
+			while (ss < se && val[se - 1].match(/\s/)) {
+				se--;
+			}
+			pb.value = val.slice(0, ss) + (val && val[ss - 1] && !val[ss - 1].match(/\s/) ? ' ' : '') + left + val.slice(ss, se) + right + (val && val [se] && !val[se].match(/\s/) ? ' ' : '') + val.slice(se);
+		} catch(error) {
+			console.error(error);
+		}
+	};
+	$c('formatting bold').onclick = basic_formatting.bind(this, '*', '*');
+	$c('formatting underline').onclick = basic_formatting.bind(this, '+', '+');
+	$c('formatting strike').onclick = basic_formatting.bind(this, '-', '-');
+	$c('formatting italic').onclick = basic_formatting.bind(this, '_', '_');
+	$c('formatting godname').onclick = basic_formatting.bind(this, '"', '":пс');
+	$c('formatting link').onclick = basic_formatting.bind(this, '"', '":');
+	var quote_formatting = function(quotation, e) {
+		try {
+			val = pb.value;
+			ss = pb.selectionStart;
+			se = pb.selectionEnd;
+			while (ss < se && val[ss].match(/\s/)) {
+				ss++;
+			}
+			while (ss < se && val[se - 1].match(/\s/)) {
+				se--;
+			}
+			pb.value = val.slice(0, ss) + (val && val[ss - 1] && !val[ss - 1].match(/\n/) ? '\n\n' : (val[ss - 2] && !val[ss - 2].match(/\n/) ? '\n' : '')) + quotation + val.slice(ss, se) + (val && val[se] && !val[se].match(/\n/) ? '\n\n' : (val[se + 1] && !val[se + 1].match(/\n/) ? '\n' : '')) + val.slice(se);
+		} catch(error) {
+			console.error(error);
+		}
+	};
+	$c('formatting bq').onclick = quote_formatting.bind(this, 'bq. ');
+	$c('formatting bc').onclick = quote_formatting.bind(this, 'bc. ');
 }
 
 } catch(e) {
-	console.log(e);
+	console.error(e);
 }
 })();
