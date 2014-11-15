3dbc7669a2d4b00151b24dff34d0030c8e09534d
zeird <l1aqus@gmail.com>
2014-11-15 21:14:11 +0600

    Add object.watch polyfill to Chrome

--- source/chrome/guip_chrome.js
+++ source/chrome/guip_chrome.js
@@ -11,3 +11,45 @@ GUIp_addGlobalStyleURL = function(url, id) {
 	sel.setAttribute('id', id);
 	document.head.appendChild(sel);
 };
+
+// object.watch polyfill
+// object.watch
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		enumerable: false,
+		configurable: true,
+		writable: false,
+		value: function(prop, handler) {
+			var oldval = this[prop];
+			var getter = function () {
+				return oldval;
+			};
+			var setter = function(newval) {
+				handler.call(this, prop, oldval, newval);
+				oldval = newval;
+			};
+
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					get: getter,
+					set: setter,
+					enumerable: true,
+					configurable: true
+				});
+			}
+		}
+	});
+}
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		enumerable: false,
+		configurable: true,
+		writable: false,
+		value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
