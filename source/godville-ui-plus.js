9d2f68021b98e235b5632af4e543400b2e0f86e7
zeird <l1aqus@gmail.com>
2014-11-16 05:20:14 +0600

    Move laying_timer inside godville-ui-plus.js

--- source/godville-ui-plus.js
+++ source/godville-ui-plus.js
@@ -1860,16 +1860,83 @@ var ui_improver = {
 	},
 
 	nodeInsertion: function() {
-		if(!ui_improver.improveInProcess) {
-			ui_improver.improveInProcess = true;
-			setTimeout(ui_improver.nodeInsertionDelay, 50);
+		if(!this.improveInProcess) {
+			this.improveInProcess = true;
+			setTimeout(this.nodeInsertionDelay.bind(this), 50);
 		}
 	},
 
 	nodeInsertionDelay: function() {
-		ui_improver.improve();
+		this.improve();
 		if (ui_data.isBattle) {
 			ui_logger.update();
+		}
+	}
+};
+
+var ui_laying_timer = {
+	init: function() {
+		// Laying timer external script
+		if (+$('#hk_bricks_cnt .l_val').text() == 1000 && !ui_data.isBattle && !ui_data.isDungeon && !ui_storage.get('Option:disableLayingTimer')) {
+			document.querySelector('#imp_button').insertAdjacentHTML('afterend', '<div id=\"laying_timer\" class=\"fr_new_badge\" />');
+			for (var key in window) {
+				if (key.match(/^diary/)) {
+					this.third_eye = key;
+					break;
+				}
+			}
+			this.tick();
+			setInterval(this.tick.bind(this), 60000);
+		}
+	},
+	tick: function() {
+		console.log('tick');
+		console.log(this.third_eye);
+		var temp, cur, first, lastLaying = 0;
+		for (var msg in window[this.third_eye]) {
+			temp = new Date(window[this.third_eye][msg].time);
+			if (msg.match(/^(?:Возложила?|Выставила? тридцать золотых столбиков)/)) {
+				lastLaying = temp > lastLaying ? temp : lastLaying;
+			}
+			if (!first || first > temp) {
+				first = temp;
+			}
+		}
+		var $timer = document.querySelector('#laying_timer');
+		$timer.classList.remove('green');
+		$timer.classList.remove('yellow');
+		$timer.classList.remove('red');
+		$timer.classList.remove('grey');
+		var hours, minutes;
+		if (lastLaying) {
+			hours = Math.floor(24 - (Date.now() - lastLaying)/1000/60/60);
+			minutes = Math.floor(60 - (Date.now() - lastLaying)/1000/60%60);
+			if (hours < 0) {
+				$timer.textContent = '✓';
+				$timer.classList.add('green');
+				$timer.title = 'Сейчас можно сделать возложение без штрафов';
+			} else {
+				$timer.textContent = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
+				if (hours >= 12) {
+					$timer.classList.add('red');
+					$timer.title = 'Сейчас на возложения действует штраф в две трети';
+				} else {
+					$timer.classList.add('yellow');
+					$timer.title = 'Сейчас на возложения действует штраф в одну треть';
+				}
+			}
+		} else {
+			if (Math.floor((Date.now() - first)/1000/60/60) >= 24) {
+				$timer.textContent = '✓';
+				$timer.classList.add('green');
+				$timer.title = 'Сейчас можно сделать возложение без штрафов';
+			} else {
+				hours = Math.floor(24 - (Date.now() - first)/1000/60/60);
+				minutes = Math.floor(60 - (Date.now() - first)/1000/60%60);
+				$timer.textContent = '?';
+				$timer.classList.add('grey');
+				$timer.title = 'Нет информации о штрафах. Неопределенность разрешится после возложения или через ' + (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes);
+			}
 		}
 	}
 };
@@ -2114,13 +2181,6 @@ var ui_starter = {
 			var shiftEnterScript = document.createElement('script');
 			shiftEnterScript.src = window.GUIp_getResource('shift_enter.js');
 			document.head.appendChild(shiftEnterScript);
-
-			// Laying timer external script
-			if (!ui_data.isBattle && !ui_data.isDungeon && !ui_storage.get('Option:disableLayingTimer')) {
-				var layingTimerScript = document.createElement('script');
-				layingTimerScript.src = window.GUIp_getResource('laying_timer.js');
-				document.body.appendChild(layingTimerScript);
-			}
 
 			// Disable PM sound external script
 			if (ui_storage.get('Option:disablePmSound')) {
