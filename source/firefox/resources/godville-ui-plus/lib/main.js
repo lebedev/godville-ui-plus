b7fe4dd6d3b299684acae1ec50295b3192675108
zeird <l1aqus@gmail.com>
2015-02-09 23:51:20 +0600

    Refactor FF loader EVEN MORE

--- source/firefox/resources/godville-ui-plus/lib/main.js
+++ source/firefox/resources/godville-ui-plus/lib/main.js
@@ -2,29 +2,20 @@ var selfUrl = require('sdk/self').data.url;
 var pageMod = require('sdk/page-mod').PageMod;
 
 function mod(url, locale) {
-	pageMod({
-		include: RegExp(url + 'superhero.*'),
-		contentScriptFile: [selfUrl('common.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('superhero.js')],
-		contentScriptWhen: 'ready'
-	});
-
-	pageMod({
-		include: RegExp(url + 'user\/(?:profile|rk_success).*'),
-		contentScriptFile: [selfUrl('common.js'), selfUrl('jquery-1.10.2.min.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('options_page.js'), selfUrl('options.js')],
-		contentScriptWhen: 'ready'
-	});
-
-	pageMod({
-		include: RegExp(url + 'forums\/show(?:_topic)?\/\\d+.*'),
-		contentScriptFile: [selfUrl('common.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('forum.js')],
-		contentScriptWhen: 'ready'
-	});
-
-	pageMod({
-		include: RegExp(url + 'duels\/log\/.*'),
-		contentScriptFile: [selfUrl('common.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('log.js')],
-		contentScriptWhen: 'ready'
-	});
+	var urls = ['superhero.*', 'user\/(?:profile|rk_success).*', 'forums\/show(?:_topic)?\/\\d+.*', 'duels\/log\/.*'],
+		scripts = [
+			[selfUrl('common.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('superhero.js')],
+			[selfUrl('common.js'), selfUrl('jquery-1.10.2.min.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('options_page.js'), selfUrl('options.js')],
+			[selfUrl('common.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('forum.js')],
+			[selfUrl('common.js'), selfUrl('guip_firefox.js'), selfUrl('phrases_' + locale + '.js'), selfUrl('log.js')]
+		];
+	for (var i = 0; i < 4; i++) {
+		pageMod({
+			include: RegExp(url + urls[i]),
+			contentScriptFile: scripts[i],
+			contentScriptWhen: 'ready'
+		});
+	}
 }
 
 mod('https?:\/\/godville.net\/', 'ru');
