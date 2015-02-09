ac01cba2fb6748ceadcbfd294560f0de4028352d
zeird <l1aqus@gmail.com>
2015-02-09 23:22:01 +0600

    Refactor Chrome loader.js

--- source/chrome/loader.js
+++ source/chrome/loader.js
@@ -8,6 +8,17 @@
 			document.head.appendChild(scr);
 		}
 	}
+	function check(path, locale) {
+		if (path.match(/^\/superhero/)) {
+			createScripts([scripts.common, scripts.guip_chrome, scripts['phrases_' + locale], scripts.superhero]);
+		} else if (path.match(/^\/user\/(?:profile|rk_success)/)) {
+			createScripts([scripts.common, scripts.jquery, scripts.guip_chrome, scripts['phrases_' + locale], scripts.options_page, scripts.options]);
+		} else if (path.match(/^\/forums\/show(?:\_topic)?\/\d+/)) {
+			createScripts([scripts.common, scripts.guip_chrome, scripts['phrases_' + locale], scripts.forum]);
+		} else if (path.match(/^\/duels\/log\//)) {
+			createScripts([scripts.common, scripts.guip_chrome, scripts['phrases_' + locale], scripts.log]);
+		}
+	}
 	var prefix = localStorage.GUIp_prefix = window.chrome.extension.getURL('');
 	var scripts = {
 		common: prefix + 'common.js',
@@ -24,24 +35,8 @@
 	var site = location.href,
 		path = location.pathname;
 	if (site.match(/^https?:\/\/godville.net/)) {
-		if (path.match(/^\/superhero/)) {
-			createScripts([scripts.common, scripts.guip_chrome, scripts.phrases_ru, scripts.superhero]);
-		} else if (path.match(/^\/user\/(?:profile|rk_success)/)) {
-			createScripts([scripts.common, scripts.jquery, scripts.guip_chrome, scripts.phrases_ru, scripts.options_page, scripts.options]);
-		} else if (path.match(/^\/forums\/show(?:\_topic)?\/\d+/)) {
-			createScripts([scripts.common, scripts.guip_chrome, scripts.phrases_ru, scripts.forum]);
-		} else if (path.match(/^\/duels\/log\//)) {
-			createScripts([scripts.common, scripts.guip_chrome, scripts.phrases_ru, scripts.log]);
-		}
+		check(path, 'ru');
 	} else if (site.match(/^https?:\/\/godvillegame.com/)) {
-		if (path.match(/^\/superhero/)) {
-			createScripts([scripts.common, scripts.guip_chrome, scripts.phrases_en, scripts.superhero]);
-		} else if (path.match(/^\/user\/(?:profile|rk_success)/)) {
-			createScripts([scripts.common, scripts.jquery, scripts.guip_chrome, scripts.phrases_en, scripts.options_page, scripts.options]);
-		} else if (path.match(/^\/forums\/show(?:\_topic)?\/\d+/)) {
-			createScripts([scripts.common, scripts.guip_chrome, scripts.phrases_en, scripts.forum]);
-		} else if (path.match(/^\/duels\/log\//)) {
-			createScripts([scripts.common, scripts.guip_chrome, scripts.phrases_en, scripts.log]);
-		}
+		check(path, 'en');
 	}
 })();
