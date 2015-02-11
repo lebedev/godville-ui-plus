eb916b823c58af1ea269ce40b169932d64804605
zeird <l1aqus@gmail.com>
2015-02-11 19:20:46 +0600

    Minor improver fix

--- source/superhero/improver.js
+++ source/superhero/improver.js
@@ -268,7 +268,7 @@ ui_improver.improveNews = function() {
 			var pet, hero_level = ui_stats.get('Level');
 			for (var i = 0; i < ui_words.base.pets.length; i++) {
 				pet = ui_words.base.pets[i];
-				if (currentMonster.toLowerCase().indexOf(pet.name) >= 0 && hero_level >= pet.min_level && hero_level <= (pet.min_level + (hasArk ? 28 : 14))) {
+				if (currentMonster.match(pet.name) && hero_level >= pet.min_level && hero_level <= (pet.min_level + (hasArk ? 28 : 14))) {
 					isTamableMonster = true;
 					break;
 				}
