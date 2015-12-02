b13d07cde8e40659dbec15402d25ded9ffdca4d3
zeird <l1aqus@gmail.com>
2015-12-02 07:48:39 +0600

    Refactor Gruntfile.js, move publish tasks outside

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+  grunt.config.init({
     pkg: grunt.file.readJSON('package.json'),
     copy: {
       chrome: {
@@ -27,18 +27,16 @@ module.exports = function(grunt) {
           {expand: true, cwd: '<%= compile_path %>/chrome/', src: ['forum.js', 'superhero.js'], dest: '<%= compile_path %>/opera/content/'}
         ]
       },
-      version: {
+      versioned: {
         options: {
-          process: function(content, srcpath) {
-            return content.replace(/\$VERSION/g, grunt.config('new_version'));
+          process: function(aContent) {
+            return aContent.replace(/\$VERSION/g, grunt.config('new_version'));
           }
         },
         files: [
           {expand: true, cwd: 'source/chrome/', src: 'manifest.json', dest: '<%= compile_path %>/chrome/'},
-          {expand: true, cwd: 'source/', src: 'superhero.js', dest: '<%= compile_path %>/chrome/'},
 
-          {expand: true, cwd: 'source/firefox/', src: ['install.rdf', 'harness-options.json'], dest: '<%= compile_path %>/firefox/'},
-          {expand: true, cwd: 'source/', src: 'superhero.js', dest: '<%= compile_path %>/firefox/data/'},
+          {expand: true, cwd: 'source/firefox/', src: 'install.rdf', dest: '<%= compile_path %>/firefox/'},
 
           {expand: true, cwd: 'source/opera/', src: 'config.xml', dest: '<%= compile_path %>/opera/'}
         ]
@@ -51,10 +49,12 @@ module.exports = function(grunt) {
                   "'use strict';\n\n",
           footer: "\n\n})();"
         },
-        src: ['source/forum/init.js',
-              'source/forum/topic_formatting.js',
-              'source/forum/topic_other.js',
-              'source/forum/main.js'],
+        src: [
+          'source/forum/init.js',
+          'source/forum/topic_formatting.js',
+          'source/forum/topic_other.js',
+          'source/forum/main.js'
+        ],
         dest: '<%= compile_path %>/chrome/forum.js'
       },
       superhero: {
@@ -62,27 +62,29 @@ module.exports = function(grunt) {
           banner: "(function() {\n" +
                   "'use strict';\n\n",
           footer: "\n\n})();",
-          process: function(content, srcpath) {
-            return content.replace(/\$VERSION/g, grunt.config('new_version'));
+          process: function(aContent) {
+            return aContent.replace(/\$VERSION/g, grunt.config('new_version'));
           }
         },
-        src: ['source/superhero/data.js',
-              'source/superhero/utils.js',
-              'source/superhero/timeout.js',
-              'source/superhero/help.js',
-              'source/superhero/storage.js',
-              'source/superhero/words.js',
-              'source/superhero/stats.js',
-              'source/superhero/logger.js',
-              'source/superhero/informer.js',
-              'source/superhero/forum.js',
-              'source/superhero/improver.js',
-              'source/superhero/inventory.js',
-              'source/superhero/timers.js',
-              'source/superhero/observers.js',
-              'source/superhero/trycatcher.js',
-              'source/superhero/starter.js',
-              'source/superhero/main.js'],
+        src: [
+          'source/superhero/data.js',
+          'source/superhero/utils.js',
+          'source/superhero/timeout.js',
+          'source/superhero/help.js',
+          'source/superhero/storage.js',
+          'source/superhero/words.js',
+          'source/superhero/stats.js',
+          'source/superhero/logger.js',
+          'source/superhero/informer.js',
+          'source/superhero/forum.js',
+          'source/superhero/improver.js',
+          'source/superhero/inventory.js',
+          'source/superhero/timers.js',
+          'source/superhero/observers.js',
+          'source/superhero/trycatcher.js',
+          'source/superhero/starter.js',
+          'source/superhero/main.js'
+        ],
         dest: '<%= compile_path %>/chrome/superhero.js'
       }
     },
@@ -94,10 +96,10 @@ module.exports = function(grunt) {
     compress: {
       chrome: {
         options: {
-          archive: 'release/godville-ui-plus@badluck.dicey_chrome.zip'
+          archive: '<%= compile_path %>/godville-ui-plus@badluck.dicey_chrome.zip'
         },
         files: [
-          {expand: true, cwd: '<%= compile_path %>/chrome', src: '**'}
+          { expand: true, cwd: '<%= compile_path %>/chrome', src: '**' }
         ]
       },
       firefox: {
@@ -106,7 +108,7 @@ module.exports = function(grunt) {
           mode: 'zip'
         },
         files: [
-          {expand: true, cwd: '<%= compile_path %>/firefox', src: '**'}
+          { expand: true, cwd: '<%= compile_path %>/firefox', src: '**' }
         ]
       },
       opera: {
@@ -115,32 +117,8 @@ module.exports = function(grunt) {
           mode: 'zip'
         },
         files: [
-          {expand: true, cwd: '<%= compile_path %>/opera', src: '**'}
+          { expand: true, cwd: '<%= compile_path %>/opera', src: '**' }
         ]
-      }
-    },
-    exec: {
-      token_request: {
-        cmd: "<%= token_request %>",
-        stdout: false,
-        stderr: false
-      },
-      sign: {
-        cmd: "mccoy -command update",
-        cwd: 'mccoy'
-      },
-      upload: {
-        cmd: "<%= upload %>"
-      },
-      update: {
-        cmd: "<%= update %>",
-        cwd: 'publish'
-      },
-      publish_chrome: {
-        cmd: "<%= publish_chrome %>"
-      },
-      publish_firefox: {
-        cmd: 'git add --all && git commit -m "Version <%= new_version %>" && git tag -a v<%= new_version %> -m "Release v<%= new_version %>" && git push --follow-tags origin master'
       }
     },
     jshint: {
@@ -258,38 +236,6 @@ module.exports = function(grunt) {
         ]
       }
     },
-    prompt: {
-      version: {
-        options: {
-          questions: [
-            {
-              config: 'new_version',
-              type: 'input',
-              message: 'Enter new version ><%= old_version %>:',
-              default: function() {
-                var version = grunt.config('old_version').split('.');
-                version[3]++;
-                return version.join('.');
-              },
-              validate: isCorrectVersion
-            }
-          ]
-        }
-      },
-      publish: {
-        options: {
-          questions: [
-            {
-              config: 'run_publish',
-              type: 'confirm',
-              message: 'Did you signed .xpi-file and want to run "publish" task?',
-              default: true,
-              validate: isCorrectVersion
-            }
-          ]
-        }
-      }
-    },
     watch: {
       scripts: {
         files: ['source/**/*', 'Gruntfile.js'],
@@ -326,6 +272,9 @@ module.exports = function(grunt) {
   });
 
   require('load-grunt-tasks')(grunt);
+  if (grunt.file.exists('publish')) {
+    grunt.loadTasks('publish');
+  }
 
   grunt.registerTask('debug', 'Compiles in debug mode.', function(browser) {
     grunt.log.ok('Compiling in debug mode.');
@@ -333,7 +282,7 @@ module.exports = function(grunt) {
     if (browser === 'firefox') {
       var build;
       try {
-        build = +grunt.file.read('debug/build') + 1;
+        build = (+grunt.file.read('debug/build') + 1) || 1;
       } catch(e) {
         build = 1;
       } finally {
@@ -350,8 +299,8 @@ module.exports = function(grunt) {
       'jshint',
       'concat',
       'copy',
-      'build_opera',
-      'build_firefox',
+      'build:opera',
+      'build:firefox',
       'update_installed_addon',
       'notify:end'
     ];
@@ -363,137 +312,25 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('update_installed_addon', 'Sends debug .xpi to FF.', function() {
-    require("request").post({url: "http://localhost:8888", body: require("fs").readFileSync("debug/godville-ui-plus@badluck.dicey.xpi")});
+    require("request").post({ url: "http://localhost:8888", body: require("fs").readFileSync("debug/godville-ui-plus@badluck.dicey.xpi") });
   });
 
-  function isCorrectVersion(value) {
-    if (!value.match(/\d+\.\d+\.\d+\.\d+/)) {
-      return false;
+
+  grunt.registerTask('build', 'Compiles extension for a specific browser.', function(aBrowser) {
+    if (!aBrowser) {
+      grunt.fatal('No browser specified');
+    } else if (!aBrowser.match(/chrome|firefox|opera/)) {
+      grunt.fatal('Wrong parameter. Possible values are: *chrome*, *firefox* and *opera*.');
     } else {
-      var new_version = value.split('.'),
-          old_version = grunt.config('old_version').split('.'),
-          isCorrect = +old_version[0] < +new_version[0] ? true :
-                      +old_version[0] > +new_version[0] ? false :
-                      +old_version[1] < +new_version[1] ? true :
-                      +old_version[1] > +new_version[1] ? false :
-                      +old_version[2] < +new_version[2] ? true :
-                      +old_version[2] > +new_version[2] ? false :
-                      +old_version[3] < +new_version[3] ? true : false;
-      if (isCorrect) {
-        grunt.file.write('new_version', value);
-      }
-      return isCorrect;
-    }
-  }
-
-  grunt.task.run('notify_hooks');
-
-  grunt.registerTask('release', 'Compiles in release mode and publishes.', function(new_version) {
-    grunt.log.ok("Compiling in release mode.");
-    if (grunt.file.exists('publish')) {
-      grunt.config.set('compile_path', 'release');
-      grunt.config.set('old_version', grunt.file.read('current_version'));
-      grunt.file.write('debug/build', 0);
       grunt.task.run([
-        'jshint'
+        'compress:' + aBrowser,
+        'clean:' + aBrowser
       ]);
-      if (new_version && isCorrectVersion(new_version)) {
-        grunt.config.set('new_version', new_version);
-        grunt.log.ok('Got new version number: ' + new_version);
-      } else {
-        grunt.task.run('prompt:version');
-      }
-      grunt.task.run([
-        'concat',
-        'copy',
-        'build_chrome',
-        'build_opera',
-        'build_firefox',
-        'prompt:publish',
-        'add_publish_task_to_queue_if_needed',
-      ]);
-
-    } else {
-      grunt.fail.warn("The required files don't exist. Can't run in 'release' mode.");
-      return false;
-    }
-  });
-
-  grunt.registerTask('publish', 'Publishes all versions.', function(new_version) {
-    grunt.log.ok("Publishing.");
-    if (grunt.file.exists('publish')) {
-      grunt.task.run([
-        'publish_chrome',
-        'publish_firefox',
-      ]);
-    } else {
-      grunt.fail.warn("The required files don't exist. Can't publish.");
-      return false;
-    }
-  });
-
-  grunt.registerTask('exec_with_token', 'Sets token to a command and runs it.', function(arg) {
-    grunt.config.set(arg, grunt.file.read('publish/' + arg).replace('$TOKEN', grunt.file.readJSON('publish/token').access_token));
-    grunt.task.run('exec:' + arg);
-  });
-
-  grunt.registerTask('build_chrome', 'Compiles Chrome extension.', function() {
-    grunt.task.run([
-      'compress:chrome',
-      'clean:chrome',
-    ]);
-  });
-
-  grunt.registerTask('publish_chrome', 'Publishes Chrome extension to Chrome Web Store.', function() {
-    grunt.config.set('token_request', grunt.file.read('publish/token_request'));
-    grunt.task.run([
-      'exec:token_request',
-      'exec_with_token:upload',
-      'exec:token_request',
-      'exec_with_token:publish_chrome'
-    ]);
-  });
-
-  grunt.registerTask('build_firefox', 'Compiles Firefox add-on.', function() {
-    grunt.task.run([
-      'compress:firefox',
-      'clean:firefox',
-    ]);
-  });
-
-  grunt.registerTask('publish_firefox', 'Publishes Firefox add-on to Github repo.', function() {
-    if (!grunt.config('new_version')) {
-      grunt.config.set('new_version', grunt.file.read('new_version'));
-    }
-    grunt.config.set('update', grunt.file.read('publish/update'));
-    grunt.task.run([
-      'exec:update',
-      'exec:sign',
-      'update_version',
-      'exec:publish_firefox'
-    ]);
-  });
-
-  grunt.registerTask('build_opera', 'Compiles Opera extension.', function() {
-    grunt.task.run([
-      'compress:opera',
-      'clean:opera'
-    ]);
-  });
-
-  grunt.registerTask('update_version', 'Updates version in "current_version" file and deletes "new_version" file.', function() {
-    grunt.file.write('current_version', grunt.file.read('new_version'));
-    grunt.file.delete('new_version');
-  });
-
-  grunt.registerTask('add_publish_task_to_queue_if_needed', 'Adds "publish" task to queue if needed.', function() {
-    if (grunt.config('run_publish')) {
-      grunt.task.run('publish');
-    } else {
-      grunt.log.ok('Run "publish" task manually when you\'re ready.');
     }
   });
 
   grunt.registerTask('default', 'watch');
+
+  grunt.task.run('notify_hooks');
 
 };
