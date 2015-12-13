945210acff68d0353a4e8d89f27809c22b9db9fc
zeird <l1aqus@gmail.com>
2015-12-14 05:56:23 +0600

    Add workaroung to startup bug in Android add-on
    See https://bugzilla.mozilla.org/show_bug.cgi?id=1231824 for the details

--- source/firefox/main.js
+++ source/firefox/main.js
@@ -2,16 +2,21 @@ var tabs = require('sdk/tabs');
 
 var domainsUrls = 'https?:\/\/(godville\\.net|gdvl\\.tk|gv\\.erinome\\.net|godvillegame\\.com)\/.*';
 
+function attachGUIpLoaderTo(tab) {
+    tab.attach({
+        contentScriptFile: './loader.js'
+    });
+}
+
 tabs.on('ready', function(tab) {
     if (tab.url.match(domainsUrls)) {
-        tab.attach({
-            contentScriptFile: './loader.js'
-        });
+        attachGUIpLoaderTo(tab);
     }
 });
 
 for (let tab of tabs) {
     if (tab.url.match(domainsUrls)) {
+        attachGUIpLoaderTo(tab);
         tab.reload();
     }
 }
