f7d9cc1772aafc430b2fded3b50013f35d3dad34
zeird <l1aqus@gmail.com>
2015-12-15 09:28:27 +0600

    Make GV tabs to reload on GUIp updates. Closes #26

---
+++ source/chrome/reload_godville_pages_on_extension_install_or_update.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.tabs.query({
+        url: [
+            "*://godville.net/*",
+            "*://godvillegame.com/*",
+            "*://gdvl.tk/duels/log/*",
+            "*://gv.erinome.net/duels/log/*"
+        ]
+    }, function(tabs) {
+        for (var i in tabs) {
+            chrome.tabs.reload(tabs[i].id);
+        }
+    });
+});
