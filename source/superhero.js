9391752888766e1f055cc60bd6d458320587a993
zeird <l1aqus@gmail.com>
2015-12-15 09:18:47 +0600

    Make superhero to load modules with async promises
    Make FF-related SVG for mask-filter to be added in FF only

--- source/superhero.js
+++ source/superhero.js
@@ -39,43 +39,20 @@ GUIp.superhero._init = function() {
     GUIp.improver.initOverrides();
 };
 GUIp.superhero.start = function() {
-    if (window.$ &&
-        (document.getElementById('m_info') || document.getElementById('stats')) &&
-        GUIp.browser &&
-        GUIp.i18n &&
-        GUIp.addCSSFromURL &&
-        GUIp.data &&
-        GUIp.forum &&
-        GUIp.help &&
-        GUIp.improver &&
-        GUIp.informer &&
-        GUIp.inventory &&
-        GUIp.logger &&
-        GUIp.observers &&
-        GUIp.stats &&
-        GUIp.storage &&
-        GUIp.timeout &&
-        GUIp.timers &&
-        GUIp.trycatcher &&
-        GUIp.utils &&
-        GUIp.words &&
-        window.so &&
-        window.so.state
-    ) {
-        clearInterval(starterInt);
-        window.console.time('Godville UI+ initialized in');
+    window.console.time('Godville UI+ initialized in');
 
-        GUIp.trycatcher.process(GUIp);
+    GUIp.trycatcher.process(GUIp);
 
-        GUIp.superhero._init();
+    GUIp.superhero._init();
 
-        if (!GUIp.data.isFight) {
-            window.onmousemove = window.onscroll = window.ontouchmove = GUIp.improver.activity;
-        }
+    if (!GUIp.data.isFight) {
+        window.onmousemove = window.onscroll = window.ontouchmove = GUIp.improver.activity;
+    }
 
+    if (GUIp.browser === 'Firefox') {
         // svg for #logger fade-out in FF
         var is5c = document.getElementsByClassName('page_wrapper_5c').length;
-        document.body.insertAdjacentHTML('beforeend',
+        document.getElementById('guip').insertAdjacentHTML('beforeend',
             '<svg id="fader">' +
                 '<defs>' +
                     '<linearGradient id="gradient" x1="0" y1="0" x2 ="100%" y2="0">' +
@@ -88,18 +65,60 @@ GUIp.superhero.start = function() {
                 '</defs>' +
             '</svg>'
         );
-
-        window.console.timeEnd('Godville UI+ initialized in');
     }
+
+    window.console.timeEnd('Godville UI+ initialized in');
 };
 
-var container = document.getElementById('guip_scripts'),
-    tagName = 'script',
-    script;
-for (var i in GUIp.superhero.modules) {
-    script = document.createElement(tagName);
-    script.src = GUIp.common.getResourceURL('modules/' + GUIp.superhero.modules[i] + '.js');
-    container.appendChild(script);
-}
+var waitFor = function(aTargetCheckFunc) {
+    return new window.Promise(function (aResolve, aReject) {
+        var ticker = 0;
+        function checkIfTargetLoaded() {
+            if (aTargetCheckFunc()) {
+                clearInterval(int);
+                aResolve();
+            }
+            if (ticker++ > 10000) {
+                aReject();
+            }
+        }
+        var int = setInterval(checkIfTargetLoaded, 50);
+        checkIfTargetLoaded();
+    });
+};
 
-var starterInt = setInterval(function() { GUIp.superhero.start(); }, 200);
+waitFor(function() {
+    return GUIp.common;
+}).then(function() {
+    var container = document.getElementById('guip'),
+        tagName = 'script',
+        script;
+    for (var i in GUIp.superhero.modules) {
+        script = document.createElement(tagName);
+        script.src = GUIp.common.getResourceURL('modules/' + GUIp.superhero.modules[i] + '.js');
+        container.appendChild(script);
+    }
+}).then(waitFor.bind(null, function() {
+    return window.$ &&
+          (document.getElementById('m_info') || document.getElementById('stats')) &&
+           GUIp.browser &&
+           GUIp.i18n &&
+           GUIp.addCSSFromURL &&
+           GUIp.data &&
+           GUIp.forum &&
+           GUIp.help &&
+           GUIp.improver &&
+           GUIp.informer &&
+           GUIp.inventory &&
+           GUIp.logger &&
+           GUIp.observers &&
+           GUIp.stats &&
+           GUIp.storage &&
+           GUIp.timeout &&
+           GUIp.timers &&
+           GUIp.trycatcher &&
+           GUIp.utils &&
+           GUIp.words &&
+           window.so &&
+           window.so.state;
+})).then(GUIp.superhero.start);
