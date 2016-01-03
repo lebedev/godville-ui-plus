ae1e9750b5874ee8b4f7616c9328c51840fb8685
zeird <l1aqus@gmail.com>
2016-01-03 23:38:57 +0600

    Make FF loader to be able to load beta module_loader

--- source/firefox/data/loader.js
+++ source/firefox/data/loader.js
@@ -1,34 +1,18 @@
 /* global self:false */
 
-var specificScripts = {
-    'superhero.*':                     'superhero.js',
-    'user\/(?:profile|rk_success).*': ['options_page.js', 'options.js'],
-    'forums\/show(?:_topic)?\/\\d+.*': 'forum.js',
-    'duels\/log\/.*':                  'log.js'
-};
-
-var attachScripts = function(aSpecificScripts) {
-    var ruUrlRegExp = 'godville\\.net|gdvl\\.tk|gv\\.erinome\\.net',
-        locale = document.location.hostname.match(ruUrlRegExp) ? 'ru' : 'en',
-        commonScriptNames = ['common.js', 'guip_firefox.js', 'phrases_' + locale + '.js'],
-        scriptNames = commonScriptNames.concat(aSpecificScripts);
-
+var validPathnames = /^\/(?:superhero|user\/(?:profile|rk_success)|forums\/show(?:_topic)?\/\\d+|duels\/log\/)/;
+if (document.location.pathname.match(validPathnames)) {
     document.body.insertAdjacentHTML('beforeend', '<div id="guip" />');
     var container = document.getElementById('guip'),
+        internalLoaderURL = 'resource://godville-ui-plus-at-badluck-dot-dicey/data/module_loader.js',
+        externalLoaderURL = 'https://raw.githubusercontent.com/zeird/godville-ui-plus/master/source/module_loader.js',
         script;
-    for (var n in scriptNames) {
-        script = document.createElement('script');
-        script.src = 'resource://godville-ui-plus-at-badluck-dot-dicey/data/' + scriptNames[n];
-        container.appendChild(script);
-    }
+
+    script = document.createElement('script');
+    script.src = window.localStorage.getItem('GUIp:beta') === 'true' ? externalLoaderURL : internalLoaderURL;
+    container.appendChild(script);
+
     script = document.createElement('script');
     script.textContent = 'window.GUIp = window.GUIp || {};\n\nGUIp.version = "' + self.options.version + '";';
     container.appendChild(script);
-};
-
-for (var pathname in specificScripts) {
-    if (document.location.pathname.match(pathname)) {
-        attachScripts(specificScripts[pathname]);
-        break;
-    }
 }
