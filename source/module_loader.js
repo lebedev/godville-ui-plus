8c3cba892d9916b7aceb155ee89d7f474ea07d1b
zeird <l1aqus@gmail.com>
2016-01-04 01:36:08 +0600

    Fix module_loader to work on /superhero in FF

--- source/module_loader.js
+++ source/module_loader.js
@@ -1,40 +1,100 @@
-// /superhero starter
+(function() {
+'use strict';
+
 window.GUIp = window.GUIp || {};
 
-GUIp.superhero = {};
+// modules_loader
+GUIp.loader = {};
 
-GUIp.superhero.modules = [
-    'trycatcher',
-    'data',
-    'storage',
-    'stats',
-    'forum',
-    'help',
-    'informer',
-    'words',
-    'inventory',
-    'logger',
-    'observers',
-    'overrider',
-    'timeout',
-    'timers',
-    'utils',
-    'improver'
-];
+GUIp.loader.section = (function() {
+    var sections = {
+        '^\/superhero'                    : 'superhero',
+        '^\/user\/(?:profile|rk_success)' : 'options',
+        '^\/forums\/show(?:_topic)?\/\\d+': 'forum',
+        '^\/duels\/log\/'                 : 'log'
+    };
 
-GUIp.superhero.start = function() {
+    for (var pathname in sections) {
+        if (window.location.pathname.match(pathname)) {
+            return sections[pathname];
+        }
+    }
+})();
+
+GUIp.loader.modules = (function() {
+    var common = [
+        'common',
+        'i18n_' + GUIp.locale
+    ];
+
+    var modules = {
+        superhero: [
+            'data',
+            'storage',
+            'stats',
+            'informer',
+            'subs',
+            'help',
+            'words',
+            'inventory',
+            'logger',
+            'observers',
+            'overrider',
+            'timeout',
+            'timers',
+            'utils',
+            'improver'
+        ],
+        options: [
+            'options_page',
+            'options'
+        ],
+        forum: [
+            'forum'
+        ],
+        log: [
+            'map_log'
+        ]
+    };
+
+    return common.concat(modules[GUIp.loader.section]);
+})();
+
+GUIp.loader.waiter = (function(){
+    var waiters = {
+        superhero: function() {
+            return window.$ &&
+                  (document.getElementById('m_info') || document.getElementById('stats')) &&
+                   window.so &&
+                   window.so.state;
+        },
+        options: function() {
+            return document.getElementById('profile_main');
+        },
+        forum: function() {
+            return true;
+        },
+        log: function() {
+            return true;
+        }
+    };
+
+    return waiters[GUIp.loader.section];
+})();
+
+GUIp.loader.start = function() {
     window.console.log('GUIp: initializing modules...');
     window.console.time('GUIp: modules initialized in');
 
-    for (var i = 0, len = GUIp.superhero.modules.length; i < len; i++) {
-        GUIp[GUIp.superhero.modules[i]].init();
+    for (var i = 0, len = GUIp.loader.modules.length; i < len; i++) {
+        GUIp[GUIp.loader.modules[i]].init();
     }
 
     if (GUIp.stats.isField()) {
         window.onmousemove = window.onscroll = window.ontouchmove = GUIp.improver.activity;
     }
 
-    if (GUIp.browser === 'Firefox') {
+    if (GUIp.browser === 'firefox') {
         // svg for #logger fade-out in FF
         var is5c = document.getElementsByClassName('page_wrapper_5c').length;
         document.getElementById('guip').insertAdjacentHTML('beforeend',
@@ -56,7 +116,7 @@ GUIp.superhero.start = function() {
     window.console.info('GUIp: started. Enjoy! :3');
 };
 
-var waitFor = function(aTargetCheckFunc) {
+GUIp.loader.waitFor = function(aTargetCheckFunc) {
     return new window.Promise(function (aResolve, aReject) {
         var ticker = 0;
         function checkIfTargetLoaded() {
@@ -73,26 +133,29 @@ var waitFor = function(aTargetCheckFunc) {
     });
 };
 
-waitFor(function() {
-    return GUIp.common && GUIp.common.loaded_specific && GUIp.common.loaded_common;
-}).then(function() {
+GUIp.loader.waitFor(function() {
     window.console.log('GUIp: v' + GUIp.version + ' is about to start.');
-    window.console.log('GUIp: loading modules: ' + GUIp.superhero.modules.join(', ') + '.');
+    window.console.log('GUIp: loading modules: ' + GUIp.loader.modules.join(', ') + '.');
     window.console.log('GUIp: waiting for modules <script>-tags to be created...');
+
     window.console.time('GUIp: created modules <script>-tags in');
     var container = document.getElementById('guip'),
         getUrl = window.localStorage.getItem('GUIp:beta') === 'true' || GUIp.browser === 'Opera' ? 'getGithubSourceURL' : 'getResourceURL',
         script;
-    for (var i = 0, len = GUIp.superhero.modules.length; i < len; i++) {
+    for (var i = 0, len = GUIp.loader.modules.length; i < len; i++) {
         script = document.createElement('script');
-        script.src = GUIp.common[getUrl]('modules/' + GUIp.superhero.modules[i] + '.js');
+        script.src = GUIp.common[getUrl]('modules/' + GUIp.loader.modules[i] + '.js');
         container.appendChild(script);
     }
     window.console.timeEnd('GUIp: created modules <script>-tags in');
+
     window.console.log('GUIp: waiting for modules to be loaded...');
+
     window.console.time('GUIp: modules loaded in');
-}).then(waitFor.bind(null, function() {
-    var modules = GUIp.superhero.modules.concat('i18n');
+
+    return true;
+}).then(GUIp.loader.waitFor.bind(null, function() {
+    var modules = GUIp.loader.modules;
     for (var i = 0, len = modules.length; i < len; i++) {
         if (!GUIp[modules[i]] || !GUIp[modules[i]].loaded) {
             return false;
@@ -103,12 +166,9 @@ waitFor(function() {
     window.console.timeEnd('GUIp: modules loaded in');
     window.console.log('GUIp: waiting for Godville to start...');
     window.console.time('GUIp: Godville started in');
-}).then(waitFor.bind(null, function() {
-    return window.$ &&
-          (document.getElementById('m_info') || document.getElementById('stats')) &&
-           window.so &&
-           window.so.state;
-})).then(function() {
+}).then(GUIp.loader.waitFor.bind(null, GUIp.loader.waiter)).then(function() {
     window.console.timeEnd('GUIp: Godville started in');
-    GUIp.superhero.start();
+    GUIp.loader.start();
 });
+
+})();
