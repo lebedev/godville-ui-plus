da8d432c8b64873b7c895602c3271be8900924e4
zeird <l1aqus@gmail.com>
2016-02-20 20:29:28 +0600

    Update Firefox loader

--- source/firefox/data/loader.js
+++ source/firefox/data/loader.js
@@ -1,35 +1,45 @@
 /* global self:false */
 
+(function() {
+
+'use strict';
+
+var prefix = 'chrome://godville-ui-plus/content/';
+sessionStorage.setItem('GUIp_prefix', prefix);
+
+var version = self.options.version;
+
 var validPathnames = /^\/(?:superhero|user\/(?:profile|rk_success)|forums\/show(?:_topic)?\/\d+|duels\/log\/)/;
 if (document.location.pathname.match(validPathnames)) {
     document.body.insertAdjacentHTML('beforeend', '<div id="guip" />');
     var container = document.getElementById('guip'),
-        internalLoaderURL = 'resource://godville-ui-plus-at-badluck-dot-dicey/data/module_loader.js',
+        internalLoaderURL = prefix + 'module_loader.js',
         externalLoaderURL = 'https://raw.githubusercontent.com/zeird/godville-ui-plus/master/source/module_loader.js',
         script;
 
     var basicScript = function() {
         window.GUIp = {};
 
+        GUIp.version = '$VERSION';
         GUIp.browser = 'firefox';
         GUIp.locale = document.location.hostname.match(/^(?:godville\.net|gdvl\.tk|gv\.erinome\.net)/) ? 'ru' : 'en';
         GUIp.common = {
             getResourceURL: function(aResName) {
-                return "chrome://godville-ui-plus/content/" + aResName;
+                return sessionStorage.getItem('GUIp_prefix') + aResName;
             },
             getGithubSourceURL: function(aPath) {
-                return "https://raw.githubusercontent.com/zeird/godville-ui-plus/master/source/" + aPath;
+                return 'https://raw.githubusercontent.com/zeird/godville-ui-plus/master/source/' + aPath;
             }
         };
     };
 
     script = document.createElement('script');
-    script.textContent =
-        '(' + basicScript.toString() + ')()\n' +
-        'GUIp.version = "' + self.options.version + '";';
+    script.textContent = '(' + basicScript.toString().replace('$VERSION', version) + ')();';
     container.appendChild(script);
 
     script = document.createElement('script');
     script.src = window.localStorage.getItem('GUIp:beta') === 'true' ? externalLoaderURL : internalLoaderURL;
     container.appendChild(script);
 }
+
+})();
